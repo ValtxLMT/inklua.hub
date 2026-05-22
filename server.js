a16f17566cdbbe34c8a8a5403d4141cba9e1ad30
Valter@MAIN <valterofc.private@gmail.com>
2026-05-22 18:59:42 -0300

    fix caminho da public

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+const publicDir = fs.existsSync(path.join(__dirname, 'public'))
+  ? path.join(__dirname, 'public')
+  : path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicDir));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(publicDir, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
